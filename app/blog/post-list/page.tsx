1494fe3452452e50004d2bfc597aa067e1480ade
qhoncheh <ghoncheh.ataee@gmail.com>
2025-01-06 12:38:18 +0330

    some edited

--- app/blog/post-list/page.tsx
+++ app/blog/post-list/page.tsx
@@ -1,5 +1,24 @@
+
+interface Post {
+    id: number,
+    title: string
+}
+
 async function PostList(){
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    const data: Post[] = await response.json()
+
+    return(
+        <div>
+            <ul>
+                {data.map((item) => (
+                    <li key={item.id}>{item.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
+
 }
 
 export default PostList
